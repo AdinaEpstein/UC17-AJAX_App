3c2df47ce9cda3d7e565465d2b1852e51d6498dd
Adina Epstein <adinaepstein@Adinas-MacBook-Pro.local>
2017-05-25 13:29:22 -0700

    complete

--- UC17.js
+++ UC17.js
@@ -1,81 +1,38 @@
-/**
- * Uses AJAX to query an internet data source for zip codes
- * @param {string} zipId The element id that has the zip code
- */
-function findZip(zipId) {
-    var long = 0;
-    var lat = 0;
-    // First get the zip code from the HTML textbox
-    var zip = document.getElementById(zipId).value;
+function findCarrier(numId) {
+    // First get the phone number from the HTML textbox
+    var num = document.getElementById(numId).value;
     // Now make a HTTP request
     var httpRequest = new XMLHttpRequest();
     httpRequest.onreadystatechange = function () {
         if (this.readyState === 4) {
             // We got a response from the server!
-            if (this.status === 200) {
+            if(this.status === 200) {
                 // The request was successful!
-                var place = JSON.parse(this.responseText);
-                lat = place.places[1];
-                long = place.places[4];
-            }
-            else if (this.status === 404) {
+                displayCarrier(this.responseText);
+            } else if (this.status === 404){
                 // No postal code found
-                displayPlace('{ "error" : "zip" }');
-            }
-            else {
+                displayCarrier('{ "carrier" : "none" }');
+            } else {
                 console.log("We have a problem...server responded with code: " + this.status);
             }
-        }
-        else {
+        } else {
             // Waiting for a response...
         }
     };
-    // Notice how the URL is appended with the zip code
-    var url = "http://api.zippopotam.us/us/" + zip;
+
+    var url = "http://apilayer.net/api/validate?access_key=4cf5d799a3b182b9344b75f4c942b47a&number=" +  num + "&country_code=&format=1";
     httpRequest.open("GET", url, true);
     httpRequest.send();
-
-    var httpRequest2 = new XMLHttpRequest();
-    httpRequest2.onreadystatechange = function () {
-        if (this.readyState === 4) {
-            // We got a response from the server!
-            if (this.status === 200) {
-                // The request was successful!
-                displayPlace();
-            }
-            else if (this.status === 404) {
-                // No postal code found
-                displayPlace('{ "error" : "data" }');
-            }
-            else {
-                console.log("We have a problem...server responded with code: " + this.status);
-            }
-        }
-        else {
-            // Waiting for a response...
-        }
-    };
-    // Notice how the URL is appended with the zip code
-    var url = "https://api.sunrise-sunset.org/json?lat="+lat+"&lng="+long+"&callback=mycallback";
-    httpRequest2.open("GET", url, true);
-    httpRequest2.send();
 }
-/**
- * Displays the zi pcode place given the JSON data
- * @param {string} data JSON data representing place for the given zip code
- */
-function displayPlace(data) { //*****change display sunset/sunrise times
+
+function displayCarrier(data){
     var place = JSON.parse(data);
-    if (place.error === "zip") {
+    var carrier = place.carrier;
+    if(place.carrier === "none") {
         document.getElementById("place").className = "alert alert-warning";
-        document.getElementById("place").innerHTML = "No place matches that zip code."
-    }
-    else if (place.error === "data") {
-         document.getElementById("place").className = "alert alert-warning";
-        document.getElementById("place").innerHTML = "Sunrise and Sunset data is not available for this zip"
-    }
-    else {
+        document.getElementById("place").innerHTML = "No carrier matches that number."
+    } else {
         document.getElementById("place").className = "alert alert-success";
-        document.getElementById("place").innerHTML = "Sunrise: " + result.sunrise + "Sunset: " + result.sunset;
+        document.getElementById("place").innerHTML = place.carrier;
     }
 }
